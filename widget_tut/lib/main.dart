bb253a1ce290fa4ead861b95bfb326a391111c44
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-02-03 02:01:30 -0800

    added sizing to 3 new images

--- widget_tut/lib/main.dart
+++ widget_tut/lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  debugPaintSizeEnabled = true; // Remove to suppress visual layout
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Layout Demo',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        // Change to buildColumn() for the other column example
+        body: Center(child: buildRow()),
       ),
     );
   }
+
+  Widget buildRow() =>
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset('images/pic1.jpg'),
+            ),
+            Expanded(
+              flex: 2,
+              child: Image.asset('images/pic2.jpg'),
+            ),
+            Expanded(
+              child: Image.asset('images/pic3.jpg'),
+            ),
+        ],
+      );
 } //MyApp
 
 
