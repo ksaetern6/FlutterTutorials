9c5abe8601b0a7973398385c6a0e17ef7b564ad0
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-02-03 01:32:57 -0800

    added basic helo world text

---
+++ widget_tut/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+} //MyApp
+
+
+
