78754da0510e813017c0933de6e7aa55f973f0c7
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-02-04 02:26:02 -0800

    added interactivty for the favorite button

--- layout_tut/lib/main.dart
+++ layout_tut/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
     /*
      * name: titleSection
      * type: Widget
+     * desc: First Column that displays the title of the Campground and Country
      */
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
@@ -49,18 +50,15 @@ class MyApp extends StatelessWidget {
           '41'. The entire row is in a Container and padded along each edge by
           32 pixels.
            */
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ], //children
       ),
     );
 
     Color color = Theme.of(context).primaryColor;
     /*
-     * name:
+     * name: buttonSection
+     * type: Widget
      */
     Widget buttonSection = Container(
       child: Row(
@@ -74,6 +72,24 @@ class MyApp extends StatelessWidget {
     );
 
     /*
+     * name: textSection
+     * type: Widget
+     * desc: Container contains the description on the page.
+     */
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    /*
      * return MaterialApp
      */
     return MaterialApp(
@@ -82,10 +98,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter Layout Demo'),
         ),
-        body: Column(
+        /*
+         * List view supports scrolling when the screen is small.
+         */
+        body: ListView(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
@@ -117,3 +143,75 @@ class MyApp extends StatelessWidget {
   }//_buildButtonColumn
 
 }//MyApp class
+
+/*
+ * name: FavoriteWidget
+ * type: class
+ * desc: public class call to create a FavoriteWidget object.
+ */
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+/*
+ * name: _FavoriteWidgetState
+ * type: class
+ * desc: class that creates the favorite star button inside of a Row. The row
+ * uses a Container to pad the size and then uses a button that calls the
+ * _isFavorite to change the state of the widget.
+ */
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            /*
+            _toggleFavorite changes the state of the IconButton when pressed.
+            Changes state by calling setState()
+             */
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        /*
+        SizedBox makes it so there isn't a discernible 'jump' when changing numbers.
+         */
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+  /*
+   * name: _toggleFavorite
+   * type: void
+   * desc: Changes the state of the _isFavorited variable and the number of
+   * _favoriteCount. Changes the state of the widget by setting the setState()
+   * function.
+   */
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited){
+        _favoriteCount -=1;
+        _isFavorited = false;
+      }//if
+      else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }//else
+    }); //setState
+  }//toggleFavorite
+
+}
